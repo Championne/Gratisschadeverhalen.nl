2071144ba4fbd9c37d71eb8cc630d4ad1d549381
Gert Jan <gert.vanstraalen@pm.me>
2026-01-24 13:40:56 +0100

    Increase header right padding to align with footer

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,7 +11,7 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-28 items-center justify-between pl-4 pr-8 lg:pr-12">
+      <div className="container mx-auto flex h-28 items-center justify-between pl-4 pr-12 lg:pr-20 xl:pr-28">
         {/* Logo */}
         <Link href="/" className="flex items-center">
           <Image 
